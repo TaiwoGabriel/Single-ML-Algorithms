08411228593e038031ca42a40497a8eccd6c31f5
Gabriel <74195306+TaiwoGabriel@users.noreply.github.com>
2022-04-03 15:53:45 +0200

    Create linear_discriminant_classifier_and_other_algorithms.py

---
+++ linear_discriminant_classifier_and_other_algorithms.py
@@ -0,0 +1,49 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+# Compare Algorithms
+import pandas
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.datasets import load_breast_cancer
+
+# load dataset
+# Importing dataset
+dataset = load_breast_cancer()
+X = dataset.data
+y = dataset.target
+
+# prepare configuration for cross validation test harness
+# prepare models
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=42)
+	cv_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
